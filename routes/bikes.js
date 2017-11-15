0f769a39890c89d7cf960f4fac21ebe32f7a2856
Kashi Rana Halma <kashi.halma@gmail.com>
2017-11-15 10:37:01 -0700

    primordial routes

---
+++ routes/bikes.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+var knex = require('../knex')
+var router = express.Router();
+const boom = require('boom')
+const jwt = require('jsonwebtoken')
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// app.get('/auth/google',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+
+// GET /auth/google/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+// app.get('/auth/google/return',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
 
 module.exports = router;
