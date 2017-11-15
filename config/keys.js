8fa4ec7de99a4b225ed8b3f0983bde75a0844529
Kashi Rana Halma <kashi.halma@gmail.com>
2017-11-15 12:22:12 -0700

    very close on the oauth

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//add to gitignore
+
+module.exports={
+  google:{
+    clientID:'1069056041791-2mrgia5duq4teqmu0dkv605m3l1q83n7.apps.googleusercontent.com',
+    clientSecret:'buMC33t7PlYszwiPTx7V5hvr'
+  }
+
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var knex = require('../knex')
 var router = express.Router();
-
+var passport = require('passport');
 
 router.get('/login', (req, res, next)=>{
   res.send('hey your logging in')
@@ -11,8 +11,8 @@ router.get('/logout', (req, res, next)=>{
   res.send('hey your logging out')
 })
 
-router.get('/google', (req, res, next)=>{
-  res.send('fuck you mother fucker')
-})
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile']
+}))
 
 module.exports = router
