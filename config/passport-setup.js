e38d0c8289ad583626614c0f48555fd7060504fd
Ken Kotch <hello@ken.uno>
2017-11-15 15:58:41 -0700

    commented out passport-setup.js

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,12 +1,12 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google').Strategy
-const keys=require('./keys');
-
-passport.use(new GoogleStrategy({
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret,
-    callbackURL: "/auth/google/redirect"
-  }, ()=>{
-
-  })
-);
+// const passport = require('passport')
+// const GoogleStrategy = require('passport-google').Strategy
+// const keys=require('./keys');
+//
+// passport.use(new GoogleStrategy({
+//     clientID: keys.google.clientID,
+//     clientSecret: keys.google.clientSecret,
+//     callbackURL: "/auth/google/redirect"
+//   }, ()=>{
+//
+//   })
+// );
