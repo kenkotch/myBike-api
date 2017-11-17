4c2badedb378bf6ef49cfcb1ee5e6db955155db1
Kashi Rana Halma <kashi.halma@gmail.com>
2017-11-17 11:06:18 -0700

    getting very close

--- routes/components.js
+++ routes/components.js
@@ -6,23 +6,35 @@ const jwt = require('jsonwebtoken')
 var bikeMileage;
 var bikeArr=[];
 router.patch('/', (req, res, next)=>{
-knex('bikes').where({id: 1}).then((bike)=>{
-  bike[0]['total_mileage']+=Number(req.body.mileage)
-  bikeMileage=[bike[0]['total_mileage']];
-  return bike[0]
-}).then((bike)=>{
-  knex('bikes').update(bike, '*').where({id: 1}).then((updatedBike)=>{
-    return updatedBike
-      }).then((updatedBike)=>{
-        knex('components').where({bike_id: 1}).then((components)=>{
-          delete components[0]['id']
-          delete components[0]['bike_id']
-          for(var i in components[0]){
-            components[0][i]-=Number(req.body.mileage)
-          }
-          return components[0]
-    }).then((components)=>{
-        knex('components').update(components, '*').where({bike_id: 1}).then((components)=>{
+req.user='sean.lemberg@gmail.com'
+
+if(!req.user){
+  res.send('user access only')
+}
+//res.send(req.user)
+var bike_id;
+
+knex('cyclists').select('id').where({email: req.user}).then((id)=>{
+  return id[0]['id']
+}).then((id)=>{
+  knex('bikes').where({id: id}).then((bike)=>{
+    bike_id=bike[0]['id']
+    bike[0]['total_mileage']+=Number(req.body.mileage)
+    bikeMileage=[bike[0]['total_mileage']];
+    return bike[0]
+  }).then((bike)=>{
+    knex('bikes').update(bike, '*').where({id: id}).then((updatedBike)=>{
+      return updatedBike
+    }).then((updatedBike)=>{
+      knex('components').where({bike_id: bike_id}).then((components)=>{
+        delete components[0]['id']
+        delete components[0]['bike_id']
+        for(var i in components[0]){
+          components[0][i]-=Number(req.body.mileage)
+        }
+        return components[0]
+      }).then((components)=>{
+        knex('components').update(components, '*').where({bike_id: bike_id}).then((components)=>{
           delete components[0]['id']
           delete components[0]['bike_id']
           bikeArr=[...components, ...bikeMileage]
@@ -32,6 +44,9 @@ knex('bikes').where({id: 1}).then((bike)=>{
 
     })
   })
+
+})
+
 })
 
 module.exports=router
