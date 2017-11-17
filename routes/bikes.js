1905c2ec9990a8176288b3cd3ffe746ae41df39b
Kashi Rana Halma <kashi.halma@gmail.com>
2017-11-17 08:57:57 -0700

    asdfasdfasdfasdfasdf

--- routes/bikes.js
+++ routes/bikes.js
@@ -6,15 +6,22 @@ const boom = require('boom')
 const jwt = require('jsonwebtoken')
 
 router.get('/', (req, res, next)=>{
+  if(!req.user){
+    res.send('user access only')
+  }
   var bikeArr;
-  knex('bikes').where({cyclist_id: 1}).then((bike)=>{
-    bikeArr=bike;
-    return bike
-  }).then((bike)=>{
-    knex('components').where({bike_id: bikeArr[0]['id']}).then((components)=>{
-      bikeArr=[...bike, ...components]
-      res.send(bikeArr)
-    })
+  // knex('bikes').where({cyclist_id: 1}).then((bike)=>{
+  //   bikeArr=bike;
+  //   return bike
+  // }).then((bike)=>{
+  //   knex('components').where({bike_id: bikeArr[0]['id']}).then((components)=>{
+  //     bikeArr=[...bike, ...components]
+  //     res.send(bikeArr)
+  //   })
+  // })
+  res.send(req.user)
+  knex('cyclists').where({email: req.user}).then((id)=>{
+    res.send(id)
   })
 })
 
