3f57025afafb2cbc35290fdb63dfcfa201ee2a09
Ken Kotch <hello@ken.uno>
2017-11-18 13:33:57 -0700

    update callbacks

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,25 +3,27 @@ var knex = require('../knex')
 var router = express.Router();
 var passport = require('passport');
 const jwt = require('jsonwebtoken')
-router.get('/login', (req, res, next)=>{
+
+router.get('/auth/login', (req, res, next)=>{
   res.send('hey your logging in')
 })
 
-router.get('/logout', (req, res, next)=>{
+router.get('/auth/logout', (req, res, next)=>{
   req.logout()
   res.send('cookeis are gone!')
 
 })
-router.get('/google',
+router.get('/auth/google',
   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'email'] }));
 
-router.get('/google/callback',
+router.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     knex('cyclists').where({email: req.user}).then((cyclist)=>{
       res.send(cyclist)
     })
   });
+
 module.exports = router
 
 // const passport = require('passport')
