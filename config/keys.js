ab148148f2cf43d85378bafc10da06369a7cf83b
Ken Kotch <hello@ken.uno>
2017-11-18 15:40:15 -0700

    revert

--- config/keys.js
+++ config/keys.js
@@ -6,8 +6,8 @@ module.exports={
     clientID:'1069056041791-2mrgia5duq4teqmu0dkv605m3l1q83n7.apps.googleusercontent.com',
     clientSecret:'buMC33t7PlYszwiPTx7V5hvr'
   },
-  // session:{
-  //   cookieKey: 'purple dreams'
-  // }
+  session:{
+    cookieKey: 'purple dreams'
+  }
 
 }
