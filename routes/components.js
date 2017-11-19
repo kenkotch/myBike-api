a89b36186512650236cdc52cc27f4732ad658214
Kashi Rana Halma <kashi.halma@gmail.com>
2017-11-19 13:16:55 -0700

    adjustments to the patches

--- routes/components.js
+++ routes/components.js
@@ -41,5 +41,62 @@ knex('cyclists').select('id').where({email: req.body.email}).then((id)=>{
     })
   })
 })
+router.patch('/tires', (req, res, next)=>{
+    if(!req.body.email){
+      res.send('user access only')
+    }
+    knex('cyclists').select('id').where({email: req.body.email}).then((id)=>{
+      return id[0]['id']
+    }).then((cyclist_id)=>{
+      knex('bikes').select('id').where({cyclist_id: cyclist_id}).then((id)=>{
+        return id[0]['id']
+      }).then((bike_id)=>{
+        knex('components').update({tires:  1500}).where({bike_id: bike_id}).then(()=>{
+          // res.send(bike_id.toString())
+          knex('components').select('tires').where({bike_id: bike_id}).then((tires)=>{
+            res.send(tires[0]['tires'].toString())
+          })
+        })
+      })
+    })
+})
+router.patch('/chain', (req, res, next)=>{
+    if(!req.body.email){
+      res.send('user access only')
+    }
+    knex('cyclists').select('id').where({email: req.body.email}).then((id)=>{
+      return id[0]['id']
+    }).then((cyclist_id)=>{
+      knex('bikes').select('id').where({cyclist_id: cyclist_id}).then((id)=>{
+        return id[0]['id']
+      }).then((bike_id)=>{
+        knex('components').update({chain:  2000}).where({bike_id: bike_id}).then(()=>{
+          // res.send(bike_id.toString())
+          knex('components').select('chain').where({bike_id: bike_id}).then((chain)=>{
+            res.send(chain[0]['chain'].toString())
+          })
+        })
+      })
+    })
+})
+router.patch('/brakes', (req, res, next)=>{
+    if(!req.body.email){
+      res.send('user access only')
+    }
+    knex('cyclists').select('id').where({email: req.body.email}).then((id)=>{
+      return id[0]['id']
+    }).then((cyclist_id)=>{
+      knex('bikes').select('id').where({cyclist_id: cyclist_id}).then((id)=>{
+        return id[0]['id']
+      }).then((bike_id)=>{
+        knex('components').update({brake_pads:  300}).where({bike_id: bike_id}).then(()=>{
+          // res.send(bike_id.toString())
+          knex('components').select('brake_pads').where({bike_id: bike_id}).then((brake_pads)=>{
+            res.send(brake_pads[0]['brake_pads'].toString())
+          })
+        })
+      })
+    })
+})
 
 module.exports=router
